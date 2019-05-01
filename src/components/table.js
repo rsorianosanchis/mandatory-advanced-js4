e96fb4c22486813ea05bb66c8ab0850e85715f70
rsoriano <rsorianosanchis@gmail.com>
2019-05-01 14:41:04 +0200

    onsdag--01/05/2019--14:40

--- src/components/table.js
+++ src/components/table.js
@@ -6,7 +6,7 @@ export function GameTable (){
   const [col]= useState(Array(7).fill(null))
   return(
     <div className='gametable'>
-      {col.map((colum,index)=>{
+      {col.map((inget,index)=>{
          return(
            <Column key={index} posColumn={index}/>
          )})}
@@ -18,21 +18,42 @@ function Column (props){
   //
   const [row] = useState(Array(6).fill(null));
   //
-  const [filledRows,fillRow] = useState([]);
-  const [selectedColumn,updateSelectedColumn] = useState(null);
+  const [filledRowsCol0,fillRowCol0] = useState(null);
+  const [filledRowsCol1,fillRowCol1] = useState(null);
+  const [filledRowsCol2,fillRowCol2] = useState(null);
+  const [filledRowsCol3,fillRowCol3] = useState([]);
+  const [filledRowsCol4,fillRowCol4] = useState([]);
+  const [filledRowsCol5,fillRowCol5] = useState([]);
+  //const [selectedColumn,updateSelectedColumn] = useState(null);
   //console.log(row);
   const addTab = (e) =>{
     let value = 0;
     console.log(e.target);
     e.preventDefault();
-    value = e.target.getAttribute('column') ;
+    value = parseInt(e.target.getAttribute('column')) ;
     console.log(`Selected column är ${value}`);
-  
-
+    //console.log(typeof(value));
+    let refPos = null;
+    switch (value) {
+      case 0:
+        console.log(`putting Tab in column 0`);
+        if (filledRowsCol0.length < 6) {
+          fillRowCol0(filledRowsCol0+1)
+          refPos = filledRowsCol0.length;
+          console.log(refPos);
+        }
+        break;
+      case 1:
+        if (filledRowsCol1.length < 6) {
+          console.log('case 1 ');
+        }
+        break;
+      default:
+    }
   }
   return(
     <div className='column'>
-      {row.map((row,index)=>
+      {row.map((inget,index)=>
         <div key= {index} className='row' >
           <div className='tab' column={props.posColumn} onClick={addTab} >
         </div>

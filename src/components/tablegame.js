13e35f9ca09597fc6b3bdf20cde04e6bb13b5f96
rsoriano <rsorianosanchis@gmail.com>
2019-05-07 12:18:24 +0200

    tisdag 12:18

--- src/components/tablegame.js
+++ src/components/tablegame.js
@@ -2,6 +2,7 @@ import React,{Component,useState } from "react";
 import './style.css';
 
 export function TableGame (){
+  //
   const [grid,updateGrid] = useState([
       [{}, {}, {}, {}, {}, {}, {}],
       [{}, {}, {}, {}, {}, {}, {}],
@@ -13,25 +14,46 @@ export function TableGame (){
     const [spelare,bytSpelare] = useState('red');
     const [vinnare,setVinnare] = useState(false);
     const [antalLekar,addLekar]= useState(0);
-    const handleClick = ()=>{
+    //
+    const handleClick = (selectedCol)=>{
+      const vgrid = [...grid];
+      console.log(vgrid);
+      for (let rad = vgrid.length-1; rad >=0; rad--){
+        if(!vgrid[rad][selectedCol].color){
+          const vspelare = spelare === 'red'?'yellow':'red';
+          vgrid[rad][selectedCol].color = vinnare;
+          if(!vinnare){
+            kontrollVinnare();
+          }
+          bytSpelare(vspelare);
+          updateGrid(vgrid);
+          addLekar(antalLekar+1);
+          return;
+        }
+      }
+    }
+    //
+    const kontrollVinnare = ()=>{
+
     }
     const handleReset = ()=>{
     }
+    //
     return(
       <div>
         <h1>4 i rad</h1>
         <h2>{vinnare
-              ?`vinnare är spelare med ${spelare}`
+              ?`vinnare är spelare med ${spelare} tabar`
               :null}
         </h2>
           <div className='bord'>
-          {grid.map((column,indexCol)=>column.map((rad,indexRad)=>{
+          {grid.map((column,indexRad)=>column.map((rad,indexCol)=>{
             return(
                 <div
                   key={`${indexCol}-${indexRad}`}
                   className={`tab`}
                   style={{background: rad.color?rad.color:'white'}}
-                  onClick={!vinnare?()=>handleClick(indexRad):null}
+                  onClick={!vinnare?()=>handleClick(indexCol):null}
                 >
                 </div>
             )
@@ -44,5 +66,3 @@ export function TableGame (){
         </div>
     )
 }
-
-//className={`tab_${indexCol}`}

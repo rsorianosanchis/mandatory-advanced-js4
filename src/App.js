d50aaf7752b99ff496f2390ec961eb4e9bfeecdd
rsoriano <rsorianosanchis@gmail.com>
2019-05-08 11:04:58 +0200

    version för leverans labb4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import {TableGame} from './components/tablegame.js';
-
+import {GameTable} from './components/table.js';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Mandatory labb 4</h1>
-        <TableGame/>
+        <h1>App sidan</h1>
+          <GameTable/>
       </div>
     );
   }
